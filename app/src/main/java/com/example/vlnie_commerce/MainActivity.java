4d19a6d9d7554220e52f36f5b41636ad6e53e49a
vlni <genbiw@gmail.com>
2024-06-10 16:17:01 +0200

    category

--- app/src/main/java/com/example/vlnie_commerce/MainActivity.java
+++ app/src/main/java/com/example/vlnie_commerce/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.vlnie_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.vlnie_commerce.adapter.CategoryAdapter;
+import com.example.vlnie_commerce.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Games"));
+        categoryList.add(new Category(2, "Sites"));
+        categoryList.add(new Category(3, "Languages"));
+        categoryList.add(new Category(4, "Other"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
